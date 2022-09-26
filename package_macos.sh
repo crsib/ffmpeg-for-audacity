d5c43236c378c353b67db506734ff28231e8b4b0
Dmitry Vedenko <dmitry@crsib.me>
2022-09-26 14:14:17 +0300

    Fixes macOS build

--- package_macos.sh
+++ package_macos.sh
@@ -3,6 +3,11 @@
 version=5.0.0
 
 export CONAN_REVISIONS_ENABLED=1
+export DEVELOPER_ID=""
+export DEVELOPER_ID_INSTALLER=")"
+export NOTARIZATION_USER_NAME=""
+export NOTARIZATION_PASSWORD=""
+
 
 scriptLocation=$(dirname "$(readlink -f "$0")")
 
@@ -33,7 +38,7 @@ cp -a ../x86_64/bin .
 
 find bin -type f -name "*.dylib" -exec lipo -create ../x86_64/{} ../arm64/{} -output {} \;
 find bin -type f -name "*.dylib" -exec lipo -info {} \;
-find bin -type f -name "*.dylib" -exec codesign -vvvv --sign "$1" --timestamp {} \;
+find bin -type f -name "*.dylib" -exec codesign --force --timestamp --sign "$DEVELOPER_ID" {} \;
 find bin -type f -name "*.dylib" -exec otool -L {} \;
 
 mv bin libs
@@ -45,6 +50,18 @@ pkgbuild --root universal \
          --identifier "com.sourceforge.audacity.ffmpegLibrariesForAudacity.pkg" \
          --version "${version}" \
          --install-location "/Library/Application Support/audacity" \
-         "output/FFmpeg_${version}_for_Audacity_on_macOS.pkg"
+         "output/FFmpeg_${version}_for_Audacity_on_macOS.pkg" \
+         --sign "$DEVELOPER_ID_INSTALLER"
 
-codesign -vvvv --sign "$1" --timestamp "output/FFmpeg_${version}_for_Audacity_on_macOS.pkg"
+# codesign --deep --timestamp --sign "$DEVELOPER_ID_INSTALLER" "output/FFmpeg_${version}_for_Audacity_on_macOS.pkg"
+
+codesign -vvvv universal/libs/libavcodec.59.18.100.dylib
+pkgutil --check-signature output/FFmpeg_${version}_for_Audacity_on_macOS.pkg
+
+xcrun altool --notarize-app \
+    --primary-bundle-id "com.sourceforge.audacity.ffmpegLibrariesForAudacity.pkg" \
+    --username "$NOTARIZATION_USER_NAME" \
+    --password "$NOTARIZATION_PASSWORD" \
+    --file "output/FFmpeg_${version}_for_Audacity_on_macOS.pkg"
+
+# xcrun altool --notarization-info 5d1dcfe3-defb-4f8d-802b-bc26728d5ceb --username "" --password ""
